df472fe1ff8b3588602a4a68e6e3230680e0c8f0
Caroly Vilo <70900275+carolyv@users.noreply.github.com>
2021-11-27 16:53:49 +0200

    Register data FE to DB

--- backend-node/src/server.js
+++ backend-node/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/User')
 const PORT = process.env.PORT || 3000
@@ -7,13 +8,8 @@ const authRoutes = require('./routes/auth')
 const categoryRoutes = require('./routes/category');
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
-
-/*app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true
-}))*/
 
 app.use('/', authRoutes)
 app.use('/category', categoryRoutes)
--- frontend-react/src/components/Register.js
+++ frontend-react/src/components/Register.js
@@ -1,22 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import axios from 'axios'
+require('dotenv').config()
+const SERVER_URL = process.env.REACT_APP_SERVER_URL
+
 
 function Register() {
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log("work")
+    const [formData, setFormData] = React.useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        initBalance: ""
+    })
+
+    const handleChange = (e) => {
+        const newFormData = { ...formData }
+        newFormData[e.target.name] = e.target.value
+        setFormData(newFormData)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        axios.post(`${SERVER_URL}/register`, {
+            firstName: formData.firstName,
+            lastName: formData.lastName,
+            email: formData.email,
+            password: formData.password,
+            initBalance: parseInt(formData.initBalance) 
+        })
+        .then((res) => {
+            console.log(res.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+        
+    }
+    
     return (
         <div className="register-form">
             <Form onSubmit={handleSubmit}>
-                <Form.Control type="text" placeholder="Eesnimi" />
-                <Form.Control type="text" placeholder="Perekonnanimi" />
-                <Form.Control type="email" placeholder="E-post" />
-                <Form.Control type="password" placeholder="Salasõna" />
-                <Form.Control type="number" placeholder="Konto summa" />
+                <Form.Control onChange={handleChange} value={formData.firstName} name="firstName" type="text" placeholder="Eesnimi" />
+                <Form.Control onChange={handleChange} value={formData.lastName} name="lastName" type="text" placeholder="Perekonnanimi" />
+                <Form.Control onChange={handleChange} value={formData.email} name="email" type="email" placeholder="E-post" />
+                <Form.Control onChange={handleChange} value={formData.password} name="password" type="password" placeholder="Salasõna" />
+                <Form.Control onChange={handleChange} value={formData.initBalance} name="initBalance" type="number" placeholder="Konto summa" />
                 <Button variant="primary" type="submit" id="register-submit-btn">Registreeru!</Button>
             </Form>
         </div>
